0c65c4edd89ebff7e69c370bb2f6b5e7b5382748
youneselhafidy <elhafidyounes4@gmail.com>
2025-12-09 21:24:41 +0100

    Add TextEmbeddings class for generating L2 normalized text embeddings using CLIP model

---
+++ backend/app/services/TextEmbeddings.py
@@ -0,0 +1,36 @@
+from torch import Tensor, no_grad
+from torch.cuda import is_available as torch_is_available
+from transformers import AutoTokenizer, CLIPTextModelWithProjection
+import config.config as Config
+
+
+class TextEmbeddings:
+    """Class to generate
+    L2 normalized text embeddings using a CLIP text model.
+    """
+    def __init__(self,model_name :str = Config.DEFAULT_CLIP_MODEL) -> None:
+        self.DEVICE = "cuda" if torch_is_available() else "cpu"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.text_model = CLIPTextModelWithProjection.from_pretrained(model_name,).to(self.DEVICE) # pyright: ignore[reportArgumentType]
+
+    def get_text_embeddings(self, texts: str) -> Tensor:
+        """Generates L2 normalized embeddings for a list of text queries."""
+        inputs = self.tokenizer(texts, padding=True, return_tensors="pt").to(self.DEVICE)
+        with no_grad():
+            # text_embeds are already normalized in this class's output
+            embeddings = self.text_model(**inputs).text_embeds 
+        return embeddings
+    
+    def get_texts_embeddings(self, texts: list[str]) -> Tensor:
+        """Generates L2 normalized embeddings for a list of text queries."""
+        inputs = self.tokenizer(texts, padding=True, return_tensors="pt").to(self.DEVICE)
+        with no_grad():
+            # text_embeds are already normalized in this class's output
+            embeddings = self.text_model(**inputs).text_embeds 
+        return embeddings
+    
+if __name__ == "__main__":
+    text_embedder = TextEmbeddings()
+    test_text = "a red image"
+    embeddings = text_embedder.get_text_embeddings(test_text)
+    print(embeddings)

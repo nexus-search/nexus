d71c3d9cd0f3163625d5007f4421896b08ace6c1
youneselhafidy <elhafidyounes4@gmail.com>
2025-12-11 10:15:16 +0100

    Refactor ImageEmbeddings class: improve image loading logic and update device check

--- backend/app/services/ImageEmbeddings.py
+++ backend/app/services/ImageEmbeddings.py
@@ -1,8 +1,8 @@
 from transformers import CLIPVisionModelWithProjection, AutoProcessor
 from PIL import Image
 from torch import Tensor, no_grad
-from torch.cuda import is_available as torch_is_available
-import config.config as Config
+from torch.cuda import is_available as cuda_is_available
+from app.config import config as Config
 import requests
 from io import BytesIO
 from os.path import splitext
@@ -11,7 +11,7 @@ class ImageEmbeddings:
     def __init__(self, model_name: str = Config.DEFAULT_CLIP_MODEL):
         self.model_name = model_name
         self.processor = AutoProcessor.from_pretrained(model_name)
-        self.DEVICE = "cuda" if torch_is_available() else "cpu"
+        self.DEVICE = "cuda" if cuda_is_available() else "cpu"
         self.vision_model = CLIPVisionModelWithProjection.from_pretrained(model_name).to(self.DEVICE) # pyright: ignore[reportArgumentType]
 
     @staticmethod
@@ -25,15 +25,14 @@ class ImageEmbeddings:
         """
         if not isinstance(image_path, str):
             raise ValueError("Image path must be a string.")
-        
-        if not splitext(image_path)[-1].lower() in ('.png', '.jpg', '.jpeg', '.bmp', '.gif'):
-            raise ValueError("Image path must point to a valid image file.")
-        
+        print(f"Loading image from: {image_path}")
         if image_path.startswith('http://') or image_path.startswith('https://'):
             response = requests.get(image_path)
             image = Image.open(BytesIO(response.content)).convert("RGB")
-        else:
+        elif splitext(image_path)[-1].lower() in ('.png', '.jpg', '.jpeg', '.bmp', '.gif'):
             image = Image.open(image_path).convert("RGB")
+        else :
+            raise ValueError("Image path must point to a valid image file.")
         return image
 
     def get_image_embeddings(self, image: Image.Image) -> Tensor:
@@ -54,15 +53,22 @@ class ImageEmbeddings:
         # Return the normalized image embeddings
         return  embeddings
     
-    def processImages(self, image_paths: list[str]) -> Tensor:
+    def processImages(self, images_paths: list[str]) -> Tensor:
+        """
+        Docstring for processImages
+        :param images_paths: links to imaegs either local paths or urls
+        :return: Tensor
+        """
         images = []
-        for image_path in image_paths:
+        for image_path in images_paths:
             image = self.__load_image(image_path)
             images.append(image)
         
         return self.get_images_embeddings_batch(images)
 
 if __name__ == "__main__":
+    import os; print("Executed from :", os.getcwd())
+    print("cuda" if cuda_is_available() else "cpu")
     image_embeddings = ImageEmbeddings()
     test_image = Image.new('RGB', (224, 224), color='red')
     embeddings = image_embeddings.get_image_embeddings(test_image)

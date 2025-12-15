f26a0475dbbf1ceb72cb4d2a8e1ee796290619bc
Khabir <hakim1khabir2@gmail.com>
2025-12-15 01:43:50 +0100

    add https security group in terraform

--- backend/infra/main.tf
+++ backend/infra/main.tf
@@ -21,7 +21,7 @@ resource "aws_key_pair" "deployer" {
 # 2. Security Group
 resource "aws_security_group" "app_sg" {
   name        = "production_firewall"
-  description = "Allow SSH and HTTP"
+  description = "Allow SSH and HTTP and HTTPS"
 
   ingress {
     description = "SSH"
@@ -35,6 +35,14 @@ resource "aws_security_group" "app_sg" {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
